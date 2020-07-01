ae4191a73a4b671f4aab20a5b14258ca3b7e8fe3
Rémi Pignol <remi.pignol@epitech.eu>
2020-07-01 16:33:35 +0200

    Add Setup of Project (dependencies)

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # UberSniff
+
+# Installation
+## libtins Installation Documentation:
+http://libtins.github.io/download/#using-windows
+
+## WinPCAP:
+https://www.winpcap.org/
--- UberSniff C++/main.cpp
+++ UberSniff C++/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
-
+#include <algorithm>
+#include <tins/tins.h>
 
 int main(int ac, char** av)
 {
+    Tins::EthernetII eth;
+    Tins::IP* ip = new Tins::IP();
+    Tins::TCP* tcp = new Tins::TCP();
 
+    // tcp is ip's inner pdu
+    ip->inner_pdu(tcp);
+
+    // ip is eth's inner pdu
+    eth.inner_pdu(ip);
 	return EXIT_SUCCESS;
 }
